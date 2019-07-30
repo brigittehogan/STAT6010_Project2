cc5280f25e8a146918f764bde9ae62f9b9f0277c
jbtiez <53019299+jbtiez@users.noreply.github.com>
2019-07-30 16:57:35 -0400

    Update Project 2.R

--- Project 2.R
+++ Project 2.R
@@ -41,11 +41,62 @@ test_reg <- glm(Count.dummy ~ TitleLength + Tquestion + Bquestion + BodyLength +
 summary(test_reg)
 anova(test_reg)
 
+##################################################################################################
 #model selection
-ols_step_all_possible(test_reg)
-plotcp1 <- regsubsets(Count.dummy ~ TitleLength + BodyLength + Tquestion + Bquestion + Timeafter2010 + homework + assignment, data = stack_q, method = "exhaustive")
-summary(plotcp1)
-plot(plotcp1, scale = "Cp")
+
+#AIC running the regression with all variables
+test_reg <- glm(Count.dummy ~ TitleLength + Tquestion + Bquestion + BodyLength + Timeafter2010 + help + TagCount + homework + assignment + BodyWordCount + TitleWordCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg) #AIC = 12369
+anova(test_reg)
+
+#AIC paring it down to only include character lengths, and not wordcounts
+test_reg1 <- glm(Count.dummy ~ TitleLength + Tquestion + Bquestion + BodyLength + Timeafter2010 + help + TagCount + homework + assignment,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg1) #AIC 12404
+
+#AIC trying with only the wordcounts, and not the character lengths
+test_reg2 <- glm(Count.dummy ~ BodyWordCount + Tquestion + Bquestion + TitleWordCount + Timeafter2010 + help + TagCount + homework + assignment,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg2) #AIC 12421
+
+#AIC trying with character lengths and no wordsearches
+test_reg3 <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + TitleLength + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg3) #AIC 12402
+
+#AIC trying with character lengths and only help wordsearch
+test_reg4 <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + TitleLength + Timeafter2010 + TagCount + homework,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg4) #AIC 12403
+
+#AIC trying without key variables
+#with no BodyLength
+test_reg5 <- glm(Count.dummy ~  Tquestion + Bquestion + TitleLength + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg5) #12420
+#with no title length
+test_reg6 <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg6) #12415
+#with no Tquestion
+test_reg7 <- glm(Count.dummy ~ BodyLength + Bquestion + TitleLength + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg7) #12408
+#with no Bquestion
+test_reg8 <- glm(Count.dummy ~ BodyLength + Tquestion + TitleLength + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg8) #12418
+#with no Tquestion
+test_reg9 <- glm(Count.dummy ~ BodyLength + Bquestion + TitleLength + Timeafter2010 + TagCount + homework,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg9) #AIC 12408
+#with no time
+test_reg10 <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + TitleLength + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg10) #AIC 13153
+#with no tags
+test_reg11 <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + TitleLength + Timeafter2010,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg11) #AIC 12406
+
+#AIC trying one more time in our likely final model with wordcount instead of character
+test_reg12 <-  glm(Count.dummy ~ BodyWordCount + Tquestion + Bquestion + TitleWordCount + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg12) #AIC 12419
+
+#Final, best mod based on AIC
+
+test_reg <- glm(Count.dummy ~ BodyLength + Tquestion + Bquestion + TitleLength + Timeafter2010 + TagCount,  data = stack_q, family = binomial(link = "logit"))
+summary(test_reg)
+##########################################################################################################################################
 
 #testing model against null and saturday
 #null
